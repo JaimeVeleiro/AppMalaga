5f49117fd65cd3c8840c3ec34820f3b1ce7838e6
JesusMadrid2005 <146918866+JesusMadrid04@users.noreply.github.com>
2024-05-25 20:39:07 +0200

    Funcionalidad de botones

--- proyecto/app/src/main/java/com/politecnicomalaga/appmalaga/Conocenos.java
+++ proyecto/app/src/main/java/com/politecnicomalaga/appmalaga/Conocenos.java
@@ -1,6 +1,9 @@
 package com.politecnicomalaga.appmalaga;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,16 @@ public class Conocenos extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_conocenos);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button botonVolver = findViewById(R.id.btVolver);
+        botonVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Conocenos.this, Inicio.class);
+                startActivity(intent);
+                finish();
+            }
         });
     }
 }
--- proyecto/app/src/main/java/com/politecnicomalaga/appmalaga/Inicio.java
+++ proyecto/app/src/main/java/com/politecnicomalaga/appmalaga/Inicio.java
@@ -1,6 +1,9 @@
 package com.politecnicomalaga.appmalaga;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,17 @@ public class Inicio extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button botonConocenos = findViewById(R.id.btConocenos);
+        botonConocenos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Inicio.this, Conocenos.class);
+                startActivity(intent);
+                finish();
+            }
         });
     }
 }
