2760ab5b05fb0abdf2d2494d5c65f1a60e44f343
Alejandro <alejandrillo10.ag@gmail.com>
2024-05-25 20:41:20 +0200

    Boton volver en opciones

--- proyecto/app/src/main/java/com/politecnicomalaga/appmalaga/Opciones.java
+++ proyecto/app/src/main/java/com/politecnicomalaga/appmalaga/Opciones.java
@@ -1,6 +1,9 @@
 package com.politecnicomalaga.appmalaga;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,15 @@ public class Opciones extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button btvolver = findViewById(R.id.btVolver);
+        btvolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Opciones.this, Inicio.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
