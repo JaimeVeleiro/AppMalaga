4190cda8bb8ac2ae93482c1428f872b72c1338e8
Jaime <jvelrui193@g.educaand.es>
2024-05-26 18:33:09 +0200

    dependencias

--- proyecto/app/build.gradle.kts
+++ proyecto/app/build.gradle.kts
@@ -32,12 +32,13 @@ android {
 
 dependencies {
 
-    implementation fileTree(dir: 'libs', include: ['*.jar'])
-    implementation (com.google.code.gson:gson:2.10.1)
+    //implementation fileTree(dir: 'libs', include: ['*.jar'])
+    //implementation(com.google.code.gson:gson:2.11.0)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
