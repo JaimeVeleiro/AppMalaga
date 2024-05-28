d565a1f26399d519ea9521f614c26fba629606e3
Jaime <jvelrui193@g.educaand.es>
2024-05-28 17:46:56 +0200

    empezamos last day

--- proyecto/app/src/main/java/com/politecnicomalaga/appmalaga/Data/Request.java
+++ proyecto/app/src/main/java/com/politecnicomalaga/appmalaga/Data/Request.java
@@ -21,20 +21,19 @@ public class Request {
     public void getData() {
         //Reseteamos el modo error para que no se guarden "errores" previos
         error = false;
-        //Instanciamos un objeto de acceso a datos, el que sabe hacer request de los datos
-        DataMovieAccess myDMA = new DataMovieAccess(this);
+
     }
 
-    public void setDataFromRESTAPI(String json) {
+    public void setData(String json) {
         //Si estamos en modo error, no hacemos nada
         if (error) return;
 
         //Procesar el JSON para pasarlo a objetos del modelo, una lista de películas o series
         Gson gson = new Gson();
-        MovieResultSet result = gson.fromJson(json, MovieResultSet.class);
+        //MovieResultSet result = gson.fromJson(json, MovieResultSet.class);
 
         //del MovieResultSet nos interesa por ahora la lista de pelis/series
-        listData = Arrays.asList(result.getResults());
+        //listData = Arrays.asList(result.getResults());
 
         //Notificamos al viewmodel
         this.vmInstance.setData();
